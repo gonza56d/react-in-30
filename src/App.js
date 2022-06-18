efd3cca1418b1a973f490ed1b8c398b0f4c27944
gonza <gonza56d@gmail.com>
2022-06-18 14:13:01 -0300

    message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import TodoList from './TodoList'
+import { v4 as uuidv4 } from 'uuid'
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    console.log(name)
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
   return (
     <>
       <TodoList todos={todos}/>
-      <input type="text" />
-      <button>Add ToDo</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add ToDo</button>
       <button>Clear completed ToDos</button>
       <div>0 left ToDo</div>
     </>
